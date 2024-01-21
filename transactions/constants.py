9dd6abeef8f2b587204fb772d2d026be08e3206a
khalid199701 <ayon201401@gmail.com>
2024-01-21 08:56:05 +0600

    Mamar Bank Code

---
+++ transactions/constants.py
@@ -0,0 +1,10 @@
+DEPOSIT = 1
+WITHDRAWL = 2
+LOAN = 3
+LOAN_PAID = 4
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+)
